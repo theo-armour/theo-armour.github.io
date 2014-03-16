f555d51d61cfe2f4da33b5cd315cefa06b87cf66
Theo Armour <t.armour@gmail.com>
2014-03-16 14:51:53 -0700

    add message.js

---
+++ message.js
@@ -0,0 +1,6 @@
+	var msg = 
+		'<hr>' +
+		'<h4>Update 2014-03-16</h4>' +
+		'<p>First post using rev 5 of <a href=http://jaanga.github.io/libs/db/index.html target="_blank">Jaanga docBrowser</a><p>' +
+	'';
+
