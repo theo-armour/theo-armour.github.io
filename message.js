494a2f0bd936901bef353227341ec94e2d445d30
Theo Armour <t.armour@gmail.com>
2014-03-16 17:19:29 -0700

    message update

--- message.js
+++ message.js
@@ -2,5 +2,7 @@
 		'<hr>' +
 		'<h4>Update 2014-03-16</h4>' +
 		'<p>First post using revision 5 of <a href=http://jaanga.github.io/libs/db/index.html target="_blank">Jaanga docBrowser</a><p>' +
+		'<p>See this week\'s presentation:<br>' +
+		'<a href=http://jaanga.github.io/events/sf-webgl-2014-03-13/readme-reader.html target="_blank">San Francisco WebG Meet-Up 2014-03-13</a>' +
 	'';
 
