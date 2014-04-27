04149e6d59c49a0b606e3799767448a8baf1667a
Theo Armour <t.armour@gmail.com>
2014-04-26 18:27:47 -0700

    new messages

--- message.js
+++ message.js
@@ -1,5 +1,9 @@
 	var msg = 
 		'<hr>' +
+
+		'<h4>Update 2014-04-26</h4>' +
+		'<p>New <a href="http://jaanga.github.io/gestification/cookbook/boilerplate/" target="_blank">Three.js Leap.js Boilerplate</a> - requires a Leap update not many people have yet. ;-(</p>' +
+		'<p>All the action is happening here <a href="http://jaanga.github.io/terrain-srtm30-plus/readme-reader.html" target="_blank">Terrain SRTM30 Plus</a>. But there\'s almost nothing to see yet</p>' +
 		'<h4>Update 2014-03-18</h4>' +
 		'<p>Cookbook entry, many new map overlays in <a href="http://jaanga.github.io/terrain-plus/" target="_blank">Jaanga Terrain Plus</a></p>' +
 		'<p>Magnetic north varies by location, altitude and time: <a href="http://fgx.github.io/fgx-airports/cookbook/display-mag/readme-reader.html" target="_blank">Display Mag</a></p>' +
