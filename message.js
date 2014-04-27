b8d985354d100351e7f06a15053b2b621e72cdb9
Theo Armour <t.armour@gmail.com>
2014-04-26 20:39:30 -0700

    typo

--- message.js
+++ message.js
@@ -9,6 +9,6 @@
 		'<p>Magnetic north varies by location, altitude and time: <a href="http://fgx.github.io/fgx-airports/cookbook/display-mag/readme-reader.html" target="_blank">Display Mag</a></p>' +
 		'<p>2014-03-16 ~ First post using revision 5 of <a href=http://jaanga.github.io/libs/db/index.html target="_blank">Jaanga docBrowser</a><p>' +
 		'<p>2014-03-13 ~ ' +
-		'<a href=http://jaanga.github.io/events/sf-webgl-2014-03-13/readme-reader.html target="_blank">San Francisco WebG Meet-Up 2014-03-13</a>' +
+		'<a href=http://jaanga.github.io/events/sf-webgl-2014-03-13/readme-reader.html target="_blank">San Francisco WebGL Meet-Up 2014-03-13</a>' +
 	'';
 
