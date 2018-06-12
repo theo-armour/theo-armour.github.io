b5c8c202428a9a08e97a66ae082d5634aa66040f
Theo Armour <theo-armour@users.noreply.github.com>
2018-06-11 21:52:36 -0700

    update readme

--- snippets/bookmarklets/theo-armour-bookmarklet.js
+++ snippets/bookmarklets/theo-armour-bookmarklet.js
@@ -18,29 +18,30 @@
 
 
 		divTooToo.innerHTML =
+
 `
+
+	<a href="https://theo-armour.github.io/snippets/bookmarklets/theo-armour-bookmarklet.html" title="on server" >theo armour bookmarklet</a>
+
+	<p><a href="javascript:(function(){var script=document.createElement('script');script.onload=function(){var stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='https://rawgit.com/mrdoob/stats.js/master/build/stats.min.js';document.head.appendChild(script);})()" title="Mr.doob's" >stat.js</a></p>
+	<p>
+		<a href="
+		javascript:(function(){var script=document.createElement('script');script.type='text/javascript';script.src='https://cdn.rawgit.com/zz85/zz85-bookmarklets/master/js/ThreeInspector.js';document.body.appendChild(script);})()
+		">zz85 three.js inspector</a>
+	</p>
+
 	<p><a href="https://theo-armour.github.io/snippets/bookmarklets/mozilla-text-editor.html" target="_blank">text edit</a></p>
 
 	<p><a href="javascript:(function(){document.body.contentEditable = true;})()" >make content editable</a></p>
 
 	<p><a href='javascript:(function(){window.open("http://jkirchartz.com/demos/HTML5notepad.html","HTML5 notepad");})()' >html5 notepad</a></p>
 
-	<p><a href='javascript:(function(){window.open("jkirchartz-demos/HTML5notepad.html","HTML5 notepad");})()' >html5 notepad</a></p>
+	<p><a href='javascript:(function(){window.open("jkirchartz-demos/HTML5notepad.html","HTML5 notepad");})()' >html5 notepad local</a></p>
 
 	<p><a href="javascript:(function(){window.open('http://validator.w3.org/check?uri='+window.location);})()" >Validate with validator.w3.org</a></p>
 
 	<p><a href="javascript:(function(){window.open('data:text/html, <html contenteditable style=padding:10%; ></html>')})()" >open new editable page</a></p>
-	<p>
-		<a href="
-			javascript:function C(v){return '<td>'+v+'</td><td>'+((v>>4).toString(16)+(v&15).toString(16)).toUpperCase()+'</td><td bgcolor=DDDDDD>&'+'#'+v+';
-			</td>';}
-			var c=4,b=Math.ceil(224/c),a='<table border=0><tr>';for(j=0;j<c;j++){a+='<td>DEC</td><td>HEX</td><td>ASC</td>';}a+='</tr>';
-			for(i=33;i<33+b;i++){a+='<tr>';
-			for(j=0;j<c;j++){t=i+(j*b);
-			if(t<=255)a+=C(t);}a+='</tr>';}a+='</table>';var W=open('','','width=500,height=600,left=0,top=0,resizable,scrollbars');
-			void(W.document.write(a));
-			">ascii table</a>
-	</p>
+
 
 	<p>
 			<a href='javascript:(function(){
@@ -58,20 +59,26 @@
 			">216 colors</a>
 	</p>
 
-
 	<p>
-
 		<a href="
-		javascript:(function(){var script=document.createElement('script');script.type='text/javascript';script.src='https://cdn.rawgit.com/zz85/zz85-bookmarklets/master/js/ThreeInspector.js';document.body.appendChild(script);})()
-		">zz85 three.js inspector</a>
+			javascript:function C(v){return '<td>'+v+'</td><td>'+((v>>4).toString(16)+(v&15).toString(16)).toUpperCase()+'</td><td bgcolor=DDDDDD>&'+'#'+v+';
+			</td>';}
+			var c=4,b=Math.ceil(224/c),a='<table border=0><tr>';for(j=0;j<c;j++){a+='<td>DEC</td><td>HEX</td><td>ASC</td>';}a+='</tr>';
+			for(i=33;i<33+b;i++){a+='<tr>';
+			for(j=0;j<c;j++){t=i+(j*b);
+			if(t<=255)a+=C(t);}a+='</tr>';}a+='</table>';var W=open('','','width=500,height=600,left=0,top=0,resizable,scrollbars');
+			void(W.document.write(a));
+			">ascii table</a>
+	</p>
+
+	<p><a href="file:///C:/" title="copy links/paste&go/or use local" >C:/</a> - <a href="file:///D:/" title="copy links/paste&go/or use local" >D:/</a><br>
+		<a href="file:///D:/Dropbox/Public/git-repos/theo-armour.github.io/snippets/bookmarks/" target="_blank">bookmarks</a><br>
+		<a href="https://github.com/theo-armour/theo-armour.github.io/edit/master/snippets/bookmarks/food.md" target="_blank" >food.md</a>
 
 	</p>
 
-	<p><a href="file:///C:/" >C:/</a> <a href="file:///D:/" >D:/</a></p>
 
-
-`
-
+`;
 
 
 	}
