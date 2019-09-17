86ed2d2f584564140b6198e4626ac4ead443838d
Theo Armour <theo-armour@users.noreply.github.com>
2019-09-16 18:43:14 -0700

    msg

--- snippets/bookmarklets/bookmarklets/capture-bookmark.js
+++ snippets/bookmarklets/bookmarklets/capture-bookmark.js
@@ -1,5 +1,5 @@
 
-var version = "0.00.00";
+var version = "0.00.01";
 var fileName;
 var domain;
 
@@ -124,7 +124,7 @@ function init() {
 function updateJson() {
 
 
-	const tags = `["${ txtTags.value.slice().replace( /,/g, '","') }"]`;
+	const tags = `${ txtTags.value.split( ",").map( item => item.trim() )
 
 	//console.log( 'document.lastModified', document.lastModified );
 
